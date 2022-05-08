1aa26f56179245e9c6d48465ddc7236aa8e80d11
Hanbo Lei <15312011bjtu@gmail.com>
2022-05-07 20:11:15 -0500

    May 7th

--- CodingCourse_Project3/test_1/script.js
+++
@@ -1,8 +0,0 @@
-const searchBar = document.querySelector("input");
-
-function printit(e) {
-    console.log(e.target.value)
-}
-
-searchBar.addEventListener("keyup", printit);
-
--- test_textSML/script.js
+++ test_textSML/script.js
@@ -2,32 +2,24 @@ let FontSizeButton = document.querySelectorAll('.FontSizeButton');
 
 let textbody = document.getElementById('testfontsizetext');
 
-alert('javascript works')
-
-
-
+//alert('javascript works')
 
 
 function largefont() {
-	alert('something'); /* https://codepen.io/celestelayne/pen/QWGOVLE?editors=1111 */
-	console.log("hello!");
 	textbody.classList.remove('mediumfont');
 	textbody.classList.remove('smallfont');
 	textbody.classList.add('largefont');
 }
 function mediumfont() {
-	console.log("hello!");
 	textbody.classList.remove('largefont');
 	textbody.classList.remove('smallfont');
 	textbody.classList.add('mediumfont');
 }
 function smallfont() {
-	console.log("hello!");
 	textbody.classList.remove('largefont');
 	textbody.classList.remove('mediumfont');
 	textbody.classList.add('smallfont');
 }
-
 
 
 FontSizeButton[0].addEventListener('click', largefont)
